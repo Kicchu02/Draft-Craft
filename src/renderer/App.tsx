2de0dccb9c901773e60b21bc758be4565bdd7ffc
maithry <pappumaithry@gmail.com>
2024-10-05 20:26:28 +0530

    Revert "Merge branch 'narayana/create-empty-page' of https://github.com/Kicchu02/Draft-Craft into maithrypappu/update-readme-file"
    This reverts commit 0feff028cab9ddbf5a4ea90194f3588afa4e70aa, reversing
    changes made to 028bdc81f168cea01df5117e5b4fcdf9cf7f539c.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,15 +1,44 @@
+import { Button, Snackbar, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
-import EmptyState from './pages/EmptyState';
 
-function Root() {
-  return <EmptyState />;
+function Hello() {
+  const [isSnackBarShown, setIsSnackBarShown] = useState(false);
+
+  return (
+    <Stack
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      gap="32px"
+    >
+      <Snackbar
+        message="Button clicked."
+        open={isSnackBarShown}
+        onClose={() => {
+          setIsSnackBarShown(false);
+        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      />
+      <Typography variant="h1">Notes App</Typography>
+      <Button
+        variant="contained"
+        size="large"
+        onClick={() => {
+          setIsSnackBarShown(true);
+        }}
+      >
+        Click Me
+      </Button>
+    </Stack>
+  );
 }
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Root />} />
+        <Route path="/" element={<Hello />} />
       </Routes>
     </Router>
   );
