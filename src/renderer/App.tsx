82046a7fe1ba05b417d06044c3f7222c085cbfee
Kicchu02 <kicchu.2002@gmail.com>
2024-10-06 11:14:12 +0530

    Reapply "Merge branch 'narayana/create-empty-page' of https://github.com/Kicchu02/Draft-Craft into maithrypappu/update-readme-file"
    This reverts commit 2de0dccb9c901773e60b21bc758be4565bdd7ffc.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,44 +1,15 @@
-import { Button, Snackbar, Stack, Typography } from '@mui/material';
-import { useState } from 'react';
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
+import EmptyState from './pages/EmptyState';
 
-function Hello() {
-  const [isSnackBarShown, setIsSnackBarShown] = useState(false);
-
-  return (
-    <Stack
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      gap="32px"
-    >
-      <Snackbar
-        message="Button clicked."
-        open={isSnackBarShown}
-        onClose={() => {
-          setIsSnackBarShown(false);
-        }}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      />
-      <Typography variant="h1">Notes App</Typography>
-      <Button
-        variant="contained"
-        size="large"
-        onClick={() => {
-          setIsSnackBarShown(true);
-        }}
-      >
-        Click Me
-      </Button>
-    </Stack>
-  );
+function Root() {
+  return <EmptyState />;
 }
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Root />} />
       </Routes>
     </Router>
   );
